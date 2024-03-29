7a5efb915d7a54daa3d8be9243e2d9bb92ca7b15
Gilles Querret <g.querret@riverside-software.fr>
2024-03-29 16:45:57 +0100

    Use new catalog generation library
    RSSW-221

--- src/assemblyCatalog.ts
+++ src/assemblyCatalog.ts
@@ -4,6 +4,8 @@ import * as path from 'path';
 import { OpenEdgeProjectConfig } from './shared/openEdgeConfigFile';
 import * as crypto from 'crypto';
 import { tmpdir } from 'os';
+import { outputChannel } from './ablStatus';
+import * as vscode from 'vscode';
 
 export function executeGenCatalog(project: OpenEdgeProjectConfig) {
     const env = process.env;
@@ -13,18 +15,31 @@ export function executeGenCatalog(project: OpenEdgeProjectConfig) {
     const cfgFile = {
         verbose: false,
         databases: [],
-        propath: [ ".builder/ls" ],
-        parameters: [ { name: "fileName", value: ".builder/catalog.json" }],
+        propath: [ path.join(__dirname, '../resources/abl-src/OpenEdge') ],
+        parameters: [
+          { name: "destFile", value: ".builder/catalog.json" },
+          { name: "pctTools", value: path.join(__dirname, '../resources/PCTTools.dll') }
+        ],
         returnValue: '',
         super: false,
         output: [],
-        procedure: 'assembliesCatalog.p'
+        procedure: 'NetAssemblyCatalog.p'
     };
     fs.writeFileSync(prmFileName, JSON.stringify(cfgFile));
     const prms = ["-clientlog", path.join(project.rootDir, ".builder/assemblyCatalog.log"), "-b", "-p", path.join(__dirname, '../resources/abl-src/dynrun.p'), "-param", prmFileName];
     if (project.gui)
         prms.push( "-basekey", "INI", "-ininame", path.join(__dirname, '../resources/abl-src/empty.ini'));
 
-    cp.spawn(project.getExecutable(), project.extraParameters.split(' ').concat(prms), { env: env, cwd: project.rootDir, detached: true });
+    outputChannel.appendLine("Assembly Catalog Generation - Command line: " + project.getExecutable() + " " + project.extraParameters.split(' ').concat(prms).join(" "));
+    const ps = cp.spawn(project.getExecutable(true), project.extraParameters.split(' ').concat(prms), { env: env, cwd: project.rootDir, detached: true });
+    ps.on('close', (code) => {
+      outputChannel.appendLine(`Assembly Catalog Generation - Process exited with code ${code}`)
+      if (code == 0) {
+        vscode.window.showInformationMessage("Assembly catalog generation completed successfully");
+      } else {
+        vscode.window.showErrorMessage("Assembly catalog generation failed, check log output");
+      }
+    });
+    
 }
 
