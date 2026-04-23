5a52331615012c254047691b0ba5f90e718eb336
Gilles Querret <g.querret@riverside-software.fr>
2026-04-23 17:33:44 +0200

    TabSize and useTabStops passed as options with 'formatting' request
    No need to pass values during initialization

--- src/extension.ts
+++ src/extension.ts
@@ -280,12 +280,9 @@ function createLanguageClient(): LanguageClient {
   };
   const serverOptions: ServerOptions = serverExec;
 
-  const ablEditorConfig = vscode.workspace.getConfiguration('editor', { languageId: 'abl' });
   // StructuredClone doesn't work with vscode workspace configuration objects, so we need to create a deep copy of the relevant configuration part
   const ablConfig = JSON.parse(JSON.stringify(vscode.workspace.getConfiguration('abl')));
   ablConfig.formatter ??= {};
-  ablConfig.formatter.tabSize = ablEditorConfig.get<number>('tabSize', 4);
-  ablConfig.formatter.useTabStops = ablEditorConfig.get<boolean>('useTabStops');
   
   // Options to control the language client
   const clientOptions: LanguageClientOptions = {
